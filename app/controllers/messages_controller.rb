38c0598f46321ad60828e3cec1cdbd647f623431
Dima <kuzmichman92@gmail.com>
2020-12-04 01:20:49 +0200

    rspec get post and response work now

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,20 +9,20 @@ class MessagesController < ActionController::API
   
   def create
     @message = Message.create!(message_params)
-    render json: message_url(@message)
-    json_response(@message, :created)
+    render json: message_url(@message), status: :created
+    # json_response(@message, :created)
   end
 
   def show
   	@message = Message.find(params[:id])
   	if @message.readed == false
   	  @message = Message.find(params[:id])
-  	  render json: @message, fields: %i[id], adapter: :json
+  	  # render json: @message, fields: %i[id], adapter: :json
   	  @message.update_attribute(:readed, true)
+      json_response(@message)
   	else
   	  render plain: "message alrady readed"
   	end
-    json_response(@message)
   end
 
   def message_params
--- spec/message_controller_spec.rb
+++ spec/message_controller_spec.rb
@@ -19,4 +19,33 @@ RSpec.describe 'Messages API', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+
+  describe 'GET /messages/:id' do
+    before { get "/messages/#{message_id}" }
+
+    context 'when the record exists' do
+      it 'returns the message' do
+        expect(response).to have_http_status(:success)
+      expect {
+        JSON.parse(response.body)
+      }.to_not raise_error
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe 'POST /messages' do
+
+    context 'when the request is valid' do
+      before { post '/messages'}
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+  end
 end
